120f8579fcfe4ded99e6713c362e6aef2e49baa9
John Rearden <reardenjohn@gmail.com>
2024-01-26 16:56:49 +0000

    Remove dj-database-url import

--- fruzzled_backend/settings.py
+++ fruzzled_backend/settings.py
@@ -16,7 +16,6 @@ import re
 import sys
 if os.path.exists('env.py'):
     import env
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
