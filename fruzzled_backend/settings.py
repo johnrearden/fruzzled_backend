7caaa636392f25872c46aaf2d012b819870c4241
John Rearden <reardenjohn@gmail.com>
2024-03-19 22:11:39 +0000

    Add (commented out) logging settings to settings.py

--- fruzzled_backend/settings.py
+++ fruzzled_backend/settings.py
@@ -229,3 +229,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom application settings
 PLAYER_PROFILE_COOKIE = 'fruzzled_profile'
+
+# DJANGO DEBUG LOGGER
+# LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': False,
+#    'handlers': {
+#        'file': {
+#            'level': 'DEBUG',
+#            'class': 'logging.FileHandler',
+#            'filename': 'debug.log',
+#        },
+#    },
+#    'loggers': {
+#        'django': {
+#            'handlers': ['file'],
+#            'level': 'DEBUG',
+#            'propagate': True,
+#        },
+#    },
+# }
