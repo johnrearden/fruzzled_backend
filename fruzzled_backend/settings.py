f5a3dd6aba6f23237c0111ce3c820dddc4a58e2c
John Rearden <reardenjohn@gmail.com>
2024-08-13 09:26:32 +0100

    Remove typo in ALLOWED_HOSTS setting

--- fruzzled_backend/settings.py
+++ fruzzled_backend/settings.py
@@ -78,7 +78,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' '),
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 INSTALLED_APPS = [
